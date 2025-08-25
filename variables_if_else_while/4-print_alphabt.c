4f4349655670fc5436ce3648e2e06580cfd78949
sondos04 <sondos.alrubaih04@gmail.com>
2025-08-25 18:37:43 +0000

    print alphabt ep. q & e by putchar

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a = 'a';
+while((a <= 'z') && (a != 'q' || a != 'e'))
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
