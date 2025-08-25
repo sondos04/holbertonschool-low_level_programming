1a1915a3897fa13f492ddbe7699561e77e935085
sondos04 <sondos.alrubaih04@gmail.com>
2025-08-25 12:56:50 +0000

    this file to check the +,-,0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -8,9 +9,9 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n < 0){
+	if(n < 0) {
 	printf("%d is negative",n);
-	} else  if(n > 0){
+	} else  if(n > 0) {
         printf("%d is positive",n);
         } else {
 	printf("%d is zero",n);
