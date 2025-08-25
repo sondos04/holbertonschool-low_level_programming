393242fdfea3ffcb464814d13d52d1b5d9900ebb
sondos04 <sondos.alrubaih04@gmail.com>
2025-08-25 15:04:24 +0000

    print the first digit of n

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,10 +10,15 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+int x = n % 10;
+if (n < 6 && n != 0)
+{printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x); }
+else if (n > 5)
+{printf("Last digit of %d is %d and is greater than 5\n", n, x); }
+else
+{printf("Last digit of %d is %d and is 0\n", n, x); }
+return (0);
 }
