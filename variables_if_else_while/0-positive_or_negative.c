637e72b2c2d7ffecabef170b8330b2afe3a67038
sondos04 <sondos.alrubaih04@gmail.com>
2025-08-25 12:52:46 +0000

    this file to check the +,-,0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -8,12 +8,12 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n<0){
-	printf("%d is negative", n);
-	} else  if(n>0){
-        printf("%d is positive", n);
+	if(n < 0){
+	printf("%d is negative",n);
+	} else  if(n > 0){
+        printf("%d is positive",n);
         } else {
-	printf("%d is zero", n);
+	printf("%d is zero",n);
 	}
 	return (0);
 }
