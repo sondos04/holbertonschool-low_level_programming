64a799a04bc488ab9569febeb3fbecc7e3b0ecbe
sondos04 <sondos.alrubaih04@gmail.com>
2025-08-26 09:55:45 +0000

    print numbers by putchar

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num = 0;
+while ( num <= 9 ){
+putchar(num);
+putchar(", ");
+num++;
+}
+putchar("$");
+putchar('\n');
+return (0);
+}
