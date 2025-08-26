9f9688eb1889f79ab2ea64b2b1c2fae6f0b62c58
sondos04 <sondos.alrubaih04@gmail.com>
2025-08-26 10:05:06 +0000

    print numbers by putchar

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,13 +6,16 @@
  */
 int main(void)
 {
-int num = 0;
-while ( num <= 9 ){
+int num;
+for (num = '0'; num <= '9'; num++)
+{
 putchar(num);
-putchar(", ");
-num++;
+if ( num != '9')
+{
+putchar(',');
+putchar(' ');
 }
-putchar("$");
-putchar('\n');
+}
+putchar('$');
 return (0);
 }
