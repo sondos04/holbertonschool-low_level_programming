a18be8fc10474af9a6b608ff904650664709c11a
sondos04 <sondos.alrubaih04@gmail.com>
2025-08-26 09:48:49 +0000

    print backwards alphabet by putchar

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -6,16 +6,18 @@
  */
 int main(void)
 {
-    int n;
-
-    for (n = 0; n < 16; n++)
-    {
-        if (n < 10)
-            putchar('0' + n);    
-        else
-            putchar('a' + (n - 10)); 
-    }
-
-    putchar('\n');
-    return (0);
+int n;
+for (n = 0; n < 16; n++)
+{
+if (n < 10)
+{
+putchar('0' + n);
+}
+else
+{
+putchar('a' + (n - 10));
+}
+}
+putchar('\n');
+return (0);
 }
