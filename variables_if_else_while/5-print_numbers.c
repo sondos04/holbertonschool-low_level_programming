682f1e27731039277652cb3251d01cfd3d731b1e
sondos04 <sondos.alrubaih04@gmail.com>
2025-08-26 07:03:26 +0000

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stido.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num = 0;
+while (num <= 10) {
+printf("%d", num);
+num++;
+}
+printf("\n");
+}
