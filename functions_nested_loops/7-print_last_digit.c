d31698e3554377b88d7655fe12f24b683bdf5fd5
sondos04 <sondos.alrubaih04@gmail.com>
2025-08-27 09:11:00 +0000

    print tha last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - this function to print the last digit
+ * @n: The integer
+ *
+ * Return: The last digit value of n
+ */
+int print_last_digit(int n)
+{
+return (n % 10);
+}
