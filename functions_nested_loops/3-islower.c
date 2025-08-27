468cdcced7fecd2fe4f720400140e5b1c1fe944e
sondos04 <sondos.alrubaih04@gmail.com>
2025-08-27 07:55:31 +0000

    check the lowercase

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,8 +1,12 @@
 #include "main.h"
-
+/**
+ * isLower -return 1 for  lowercase and 0 for other
+ *
+ * Return: 1 or 0
+ */
 int _islower(int c)
 {
-if (c )
+if (c >= 'a' && c <= 'z' )
 {
 return (1);
 }
