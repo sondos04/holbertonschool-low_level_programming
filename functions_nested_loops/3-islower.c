bcab7b23d1be2385d5257679ea9c2936cae4c506
sondos04 <sondos.alrubaih04@gmail.com>
2025-08-27 08:03:08 +0000

    check the lowercase

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - checks if a character is lowercase
- * c: the character to check
+ * @c: the character to check
  *
  * Return: 1 if c is lowercase, 0 otherwise
  */
