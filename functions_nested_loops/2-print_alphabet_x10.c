a7426edcb08d045a38eb7a65d51ea59702b47f47
sondos04 <sondos.alrubaih04@gmail.com>
2025-08-27 07:29:04 +0000

    print alaphbet 10 times

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- * print_alphabet - Prints the lowercase alphabet followed by a new line
+ * print_alphabet_x10 - Prints the lowercase alphabet followed by a new line 10 times
  *
  * Return: Nothing
  */
 void print_alphabet_x10(void)
 {
 int num;
-char a = 'a' ;
-for ( num = 1; num <= 10; num++ )
+char a = 'a';
+for (num = 1; num <= 10; num++)
 {
-while ( a <= 'z' )
+while (a <= 'z')
 {
 _putchar(a);
 a++;
 }
 _putchar('\n');
-a = 'a' ;
+a = 'a';
 }
 }
