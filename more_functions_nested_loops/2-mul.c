a435c7cf56f213fc01afecc7834e8ab7f1666363
sondos04 <sondos.alrubaih04@gmail.com>
2025-08-31 17:34:46 +0000

    multi two int

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,4 +1,13 @@
 #include "main.h"
+/**
+ * mul - Multiplication two digit
+ * @a: The character to multiplication
+ * @b: The character to multiplication
+ *
+ * Description: This function multiplication two input character
+ *
+ * Return: result of multiplicaton
+ */
 int mul(int a, int b)
 {
 return (a * b);
