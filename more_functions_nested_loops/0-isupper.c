b8c5fa09a6d431e7e360431a359853211784d716
sondos04 <sondos.alrubaih04@gmail.com>
2025-08-31 12:58:14 +0000

    check the upper case

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * isupper - check the uppercase alphabet
+ * c - to check uppercase
+ *
+ * Return: 1 if c uppercase and 0 otherwise
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return 1;
+}
+else
+{
+return 0;
+}
+}
