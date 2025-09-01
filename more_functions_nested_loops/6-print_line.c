6d6eb19553b884f231184aa8b4422b2ac55cd106
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-01 12:56:15 +0000

    print _

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,12 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character '_' should be printed
+ *
+ * Description: If n is 0 or less, the function should only print \n.
+ * Return: void
+ */
 void print_line(int n)
 {
 int cunt;
