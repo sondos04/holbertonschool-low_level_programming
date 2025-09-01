836920c2e83881fc70f8a9c3722879c52ba197f1
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-01 18:33:12 +0000

    print #

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character '_' should be printed
+ *
+ * Description: If n is 0 or less, the function should only print \n.
+ * Return: void
+ */
+void print_square(int size)
+{
+int cunt;
+if (size > 0)
+{
+for (cunt = 0; cunt < size ; cunt++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+}
