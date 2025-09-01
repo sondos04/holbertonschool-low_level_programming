097af0e5d3e1e757393abfaee2a9aff53b7bef10
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-01 19:47:41 +0000

    print triangle #

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a right-aligned triangle using #
+ * @size: The height of the triangle
+ */
+void print_triangle(int size)
+{
+int row, space, hash;
+if (size <= 0)
+{
+_putchar('\n');   
+}
+for (row = 1; row <= size; row++)
+{
+for (space = size - row; space > 0; space--)
+_putchar(' ');
+for (hash = 1; hash <= row; hash++)
+_putchar('#');
+_putchar('\n');
+}
+}
