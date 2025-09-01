c4c5692b4c0b5056b91d13263782618ab27ca41b
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-01 19:26:33 +0000

    prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int count;
+for (count = 1; count <= 100; count++)
+{
+if(count % 3 == 0 && count % 5 == 0)
+pritnf("FizzBuzz");
+else if (count % 3 == 0)
+printf("Fizz");
+else if (count % 5 == 0)
+printf("Buzz");
+else
+prritf("%d", count);
+if (count != 100)
+printf(" ");
+}
+printf("\n");
+return(0);
+}
