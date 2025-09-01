27f3c57f17fe9fe3c555b63dd9f42213cbcb43e6
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-01 12:24:00 +0000

    print _

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 void print_line(int n)
 {
 int cunt = 0;
 while (n != 0)
 {
-while (cunt < n)
+for (cunt = 0; cunt < n ; cunt++)
 {
-_putchar('_');
-cunt++;
+_putchar(95);
 }
 _putchar('\n');
 }
