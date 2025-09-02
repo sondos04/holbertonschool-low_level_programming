d7184ff18cd441cbfc7fecabed62c5d56a2dd66f
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-02 20:16:15 +0000

    readme file

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-/*
- * reset_to_98 - function to reset pointer n to be 98
+/**
+ * reset_to_98 - Updates the value of the integer pointed to by n to 98.
+ * @n: Pointer to an int to update
  *
- *@n= pointer to interger
+ * Description: This function takes a pointer to an integer as a parameter
+ * and sets the value it points to to 98.
  */
 void reset_to_98(int *n)
 {
