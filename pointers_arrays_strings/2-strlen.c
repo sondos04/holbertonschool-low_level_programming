2f11356b7fb488f39f00e803f2eef2927dacd5d4
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-02 23:15:44 +0000

    length of string

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * _strlen - Returns the length of a string
+ * @s: The string to measure
+ *
+ * Return: The length of the string
+ */
 int _strlen(char *s)
 {
 int count = 0;
