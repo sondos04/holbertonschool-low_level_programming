0bcd477d123c959e9944b9a822810b557ce7c9be
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-02 23:11:32 +0000

    main task 2

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-    char *str = "Holberton!";
+    char *str;
     int len;
 
+    str = "My first strlen!";
     len = _strlen(str);
     printf("%d\n", len);
-
     return (0);
 }
