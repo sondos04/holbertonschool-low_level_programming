7336f5cb50d934d3517fe03468f450bc46b41ada
Your Name <you@example.com>
2025-09-03 08:59:47 +0000

    print numbers

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_array - function prints n elements of an array of integers
+ * @a: array of integers
+ * @n: number of elements to print
+ *
+ * Description: prints the elements of array a, separated by comma and space,
+ * followed by a new line. Only the first n elements are printed.
+ */
 void print_array(int *a, int n)
 {
 int i = 0;
