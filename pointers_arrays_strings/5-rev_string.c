f5c41427912806a7c0738a11ea16fad125b09f74
Your Name <you@example.com>
2025-09-03 07:58:37 +0000

    revers string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -2,14 +2,16 @@
 #include "main.h"
 void rev_string(char *s)
 {
-    int len = 0;
+int len = 0;
 int i;
-    while (s[len] != '\0') {
-        len++;
-    }
-    for (i = 0; i < len / 2; i++) {
-        char temp = s[i];
-        s[i] = s[len - i - 1];
-        s[len - i - 1] = temp;
-    } 
+while (s[len] != '\0')
+{
+len++;
+}
+for (i = 0; i < len / 2; i++)
+{
+char temp = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = temp;
+}
 }
