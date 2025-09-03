38a89318988493ac13b4c95af3b246a304601017
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-03 21:51:34 +0000

    cpoy string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 char *_strcpy(char *dest, char *src)
 {
-	int i;
-	while (scr[i] != '\0')
-	{
-		dest[i] = scr[i];
-	i++;
+int i;
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
 }
 dest[i] = '\0';
-	return dest;
+return dest;
 }
