5f32dd0195d3eb2b0616a1312c80dabb16c78d96
Your Name <you@example.com>
2025-09-03 08:25:52 +0000

    print even numbers of string

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,17 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdio.hi>
+/**
+ * puts2 - function to prints every other character of a string, starting with the first
+ * @str: input string
+ */
 void puts2(char *str)
 {
-	int i = 0;
-	while (str[i] != '\0')
-	{
-	if (i % 2 == 0)
-		_putchar(str[i]);
-	i++;
-	}
-	_putchar('\n');
+int i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
 }
