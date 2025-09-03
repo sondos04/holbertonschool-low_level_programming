5090cc61d644138929d593e01b8ee05b1b8c5170
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-03 21:44:26 +0000

    cpoy string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -3,9 +3,11 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+	char temp;
 	for (i = 0 ; i >= '\0' ; i++)
 	{
-		dest [i] = src[i];
+		dest[i] = src[i];
+		src[i] = temp[i];
 	}
-	return dest;
+	return temp;
 }
