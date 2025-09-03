70f2702f662c80f970bb0ab482218aaf4a063f82
Your Name <you@example.com>
2025-09-03 07:08:12 +0000

    print text revers

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -3,12 +3,13 @@
 #include <string.h>
 void print_rev(char *s)
 {
-while (*s != '\0')
-s++;
-while ( s >= 0 )
+char *end = s;
+while (*end != '\0')
+end++;
+while ( end >= 0 )
 {
-_putchar(*s);
-s--;
+_putchar(*end);
+end--;
 }
 _putchar('\n');
 }
