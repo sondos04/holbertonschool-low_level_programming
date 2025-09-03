6607e41adf8f496d161eaed836bf3ecb0391a594
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-03 21:58:39 +0000

    cpoy string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _strcpy - function copies the string pointed to by src (including the null byte)
+ *           to the buffer pointed to by dest.
+ * @dest: Destination buffer.
+ * @src: Source string.
+ *
+ * Return: The pointer to dest.
+ */
 char *_strcpy(char *dest, char *src)
 {
 int i;
