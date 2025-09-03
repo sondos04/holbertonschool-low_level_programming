cdcac553702a4cee9df0873fb644cb678e2d14e1
Your Name <you@example.com>
2025-09-03 06:34:46 +0000

    print pinter by putchar

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: pointer to the string to print
+ *
+ * Return: void
+ */
 void _puts(char *str)
 {
 while (*str != '\0')
