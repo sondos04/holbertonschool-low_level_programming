00a04a68c05d7a65718699528070198ead881eb8
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-03 22:15:34 +0000

    print char as int

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,31 @@
 #include "main.h"
-#include <stdio.h>
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to convert.
+ *
+ * Return: The converted integer. If no number found, returns 0.
+ */
 int _atoi(char *s)
 {
-	int new = printf("%s" , s);
-return (new);
+int i = 0;
+int sign = 1;
+int result = 0;
+int started = 0;
+while (s[i] != '\0')
+{
+if (s[i] == '-')
+sign *= -1;
+else if (s[i] >= '0' && s[i] <= '9')
+{
+started = 1;
+result = result * 10 + (s[i] - '0');
+}
+else if (started)
+{
+break;
+}
+i++;
+}
+return (result * sign);
 }
