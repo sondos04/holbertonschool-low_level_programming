1b647879e8ab6afc2904d8c757016bc3fda70f11
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-03 22:18:57 +0000

    print char as int

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,7 @@
 int _atoi(char *s)
 {
 int i = 0;
-int sign = 1;
+iint sign = 1;
 int result = 0;
 int started = 0;
 while (s[i] != '\0')
@@ -22,10 +22,10 @@ started = 1;
 result = result * 10 + (s[i] - '0');
 }
 else if (started)
-{
 break;
-}
 i++;
 }
-return (result * sign);
+if (sign == 1)
+result = -result;
+return (result);
 }
