fc94c9d920e496595ed88721c9fca198abb8d127
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-04 22:47:25 +0000

    overwrite string

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,17 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _strcat -function concatenates two strings
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ *
+ * Description: This function appends the string pointed to by @src
+ * to the end of the string pointed to by @dest. The terminating
+ * null byte (\0) at the end of @dest is overwritten, and a new
+ * null byte is added at the end of the result.
+ *
+ * Return: pointer to the resulting string @dest
+ */
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
