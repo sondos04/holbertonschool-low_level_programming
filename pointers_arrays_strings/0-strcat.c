2ac6f8ddf1786b4d50b2e824b0c5f3f416db127c
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-04 22:50:42 +0000

    overwrite string

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -16,6 +16,7 @@ char *_strcat(char *dest, char *src)
 {
 	int i = 0;
 	int j = 0;
+
 	while (dest[i] != '\0')
 		i++;
 	while (src[j] != '\0')
@@ -26,6 +27,4 @@ char *_strcat(char *dest, char *src)
 	}
 	dest[i] = '\0';
 	return (dest);
-	
-
 }
