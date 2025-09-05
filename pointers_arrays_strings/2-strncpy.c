96c3b039466aebb466717d3d1c860604450171fe
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-05 18:40:48 +0000

    replace string to another

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncat - function concatenates two strings
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ * @n: numbers of bytes from src
+ *
+ *
+ * Return: pointer to the resulting string @dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
