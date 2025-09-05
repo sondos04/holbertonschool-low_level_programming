4ee1f2915e72ac7084a6d6c927c7d23e74c4079b
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-05 20:10:44 +0000

    diff int of 2 strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _strcmp - function calculate the diff between two strings
+ *@s1 = pointer of string one
+ @s2 = pointer of string two
+
+ * Return: the diffrente of two strings.
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-    while (s1[i] && s2[i])
-    {
-        if (s1[i] != s2[i])
-            return s1[i] - s2[i];
-        i++;
-    }
-
-    return s1[i] - s2[i];  
+int i = 0;
+while (s1[i] && s2[i])
+{
+if (s1[i] != s2[i])
+return s1[i] - s2[i];
+i++;
+}
+return (s1[i] - s2[i]);  
 }
