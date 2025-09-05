ca5f944787d9987d579349ab09f1b5f1e15aab85
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-05 19:19:49 +0000

    diff int of 2 strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,9 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int count1 = *s1[];
-	int count2 = *s2[];
-	int diff = count1 - count2;
+	int count1 = 0;
+	int count2 = 0;
+	int diff;
+	while (s1[count1] != '\0')
+		count1++;
+	 while (s2[count2] != '\0')
+                count2++;
+	diff = count1 - count2;
 	return (diff);
 
 }
