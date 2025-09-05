fe627777ff8541d2ca4c6da76cce67d2e3e71390
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-05 22:29:45 +0000

    reverse array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -10,12 +10,12 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i;
-	int temp;
-	   for (i = 0; i < n / 2; i++)
-    {
-        temp = a[i];
-        a[i] = a[n - i - 1];
-        a[n - i - 1] = temp;
-    }
+int i;
+int temp;
+for (i = 0; i < n / 2; i++)
+{
+temp = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = temp;
+}
 }	
