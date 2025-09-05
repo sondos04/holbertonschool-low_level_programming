8714851f8177f9ef369dfd851c73a973ecf73870
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-05 20:28:38 +0000

    reverse array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	while (i != '\0')
+		i++;
+	while (i >= 0)
+	{
+		printf("%d",a[i]);
+		i--;
+	}
+}
