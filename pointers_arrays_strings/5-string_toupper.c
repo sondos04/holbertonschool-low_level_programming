36ba2643f9f22fd7d6eab13355425c703ed4a4da
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-06 00:16:08 +0000

    main headers

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -8,14 +8,12 @@
  */
 char *string_toupper(char *s)
 {
-    int i = 0;
-
-    while (s[i] != '\0')
-    {
-        if (s[i] >= 'a' && s[i] <= 'z')
-            s[i] = s[i] - 32;
-        i++;
-    }
-
-    return (s);
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+s[i] = s[i] - 32;
+i++;
+}
+return (s);
 }
