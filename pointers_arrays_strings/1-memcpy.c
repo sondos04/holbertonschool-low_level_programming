4dec84b11ac2db6bcb9a5f9f3b91f12aa13c4c45
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-07 12:20:43 +0000

    copy memory to another

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	i = 0;
+	while (i <= n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return(dest);
+}
