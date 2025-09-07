65c060aa7935a40fe87bd8de5cec7460a23d2489
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-07 12:26:11 +0000

    copy memory to another

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,21 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _memcpy - fills memory with a another memory for n byte
+ * @dest: pointer to the memory area to replace
+ * @src: pointer to the memory area to fill
+ * @n: number of bytes to replace
+ *
+ * Return: a pointer to the memory area dest
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-	i = 0;
-	while (i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	return(dest);
+unsigned int i;
+i = 0;
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
+return(dest);
 }
