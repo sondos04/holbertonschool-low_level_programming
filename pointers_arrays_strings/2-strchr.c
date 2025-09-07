22af7dcdc8aa7e142073984a66ffd18412f6c54b
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-07 13:09:25 +0000

    print when string start with char c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,15 +1,27 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _strchr - locates the first occurrence of a character in a string
+ * @s: pointer to the string to search
+ * @c: character to locate
+ *
+ * Return: pointer to the first occurrence of c in s,
+ *         or NULL if the character is not found
+ */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-	while(s[i] != '\0')
-	{
-	if (s[i] == c)
-	{
-		return (&s[i]);
-	}
-	i++;
-	}
-	return(NULL);
+int i = 0;
+while(s[i] != '\0')
+{
+if (s[i] == c)
+{
+return (&s[i]);
+}
+i++;
+}
+if (c == '\0')
+{
+return (&s[i]);
+}
+return (NULL);
 }
