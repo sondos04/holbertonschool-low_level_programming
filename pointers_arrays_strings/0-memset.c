88aca13cf55b3a55311a08708f1f02ca912153aa
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-07 12:08:45 +0000

    replace *s value by b for n time

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _memset - function fills memory with a constant byte
+ * @s: pointer to the memory area to fill
+ * @b: the byte to fill the memory with
+ * @n: number of bytes to fill
+ *
+ * Return: a pointer to the memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i = 0;
