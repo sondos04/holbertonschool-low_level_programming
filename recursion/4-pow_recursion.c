603c4679a2afa37ffc790a3c6c6359d3770eae26
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-09 22:29:29 +0000

    print power

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,10 +1,17 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _pow_recursion - Returns the x ^ y
+ * @x: The base
+ * @y: the power of the base
+ *
+ * Return: The resulte of the power
+ */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	 if (y == 0)
-                return (1);
+	if (y == 0)
+		return (1);
 	return (x * _pow_recursion(x , (y - 1)));
 }
