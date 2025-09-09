346b968ce763eec8feb3aa1849bfc237223cb3e5
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-09 20:37:45 +0000

    print reverse

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -3,10 +3,8 @@
 #include <unistd.h>
 void _print_rev_recursion(char *s)
 {
-	int i = 0;
-	while (s[i] != '\0')
-		i++;
-	 write(1, s, 1);
-	_print_rev_recursion(s - 1);
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	write(1, s, 1);
 }
-
