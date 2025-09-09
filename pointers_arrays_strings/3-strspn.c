3a9166d793e1bb2534687afdc20b172b8ce09fb0
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-09 07:11:11 +0000

    count similar letters

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _strspn -function that gets the length of a prefix substring
+ * @s: the string to search in
+ * @accept: the string containing the characters to match
+ *
+ * Return: number of bytes in the initial segment of s
+ *         which consist only of bytes from accept
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int j;
