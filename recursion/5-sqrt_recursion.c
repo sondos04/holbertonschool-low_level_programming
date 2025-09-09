44f9440f0ed2067db56bc8615abacea9f9a8a0d2
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-09 23:23:15 +0000

    return sqrt of n

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,11 +1,29 @@
 #include "main.h"
 #include <stdio.h>
-#include <math.h>
+/**
+ * sqrt_helper - recursively finds the natural square root of n
+ * @n: the number to find the square root of
+ * @guess: current guess to test
+ *
+ * Return: the natural square root of n if it exists, -1 otherwise
+ */
+static int sqrt_helper(int n, int guess)
+{
+    if (guess * guess == n)
+        return guess;
+    if (guess * guess > n)
+        return -1;
+    return sqrt_helper(n, guess + 1);
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to find the square root of
+ *
+ * Return: the natural square root of n if it exists, -1 otherwise
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n % 2 != 0)
-		return (-1);
-	return (sqrt(n));
+	return sqrt_helper(n, 1);
 }
