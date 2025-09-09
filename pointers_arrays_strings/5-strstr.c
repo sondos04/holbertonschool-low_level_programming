a7b45e6389a9ff8ebfa445cb0c549e3aa59eb01d
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-09 08:19:03 +0000

    print first letter not similar

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strpbrk - function to searches a string for any of a set of bytes
- * @s: the string to be scanned
- * @accept: the string containing the characters to match
+ * _strstr - function to searches a string for any of a set of bytes
+ * @haystack: the string to be scanned
+ * @needle: the string containing the characters to match
  *
- * Return: pointer to the first occurrence in s of any of the bytes in accept,
+ * Return: first occurrence of the substring needle in the string,
  * or NULL if no byte is found
  */
 char *_strstr(char *haystack, char *needle)
