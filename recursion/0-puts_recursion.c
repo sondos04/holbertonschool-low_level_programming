e3646852e88c95610241a3a4b5382eeaa05aaf14
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-09 19:52:04 +0000

    print by putchar

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,6 +1,17 @@
-#include "main.h"
 #include <stdio.h>
-void _puts_recursion(char *s)
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: pointer to the string to print
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s);
 {
-		putchar(s);
+while (*s != '\0')
+{
+_putchar(*s);
+s++;
+}
+_putchar('\n');
 }
