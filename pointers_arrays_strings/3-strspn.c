74a79fd0c9f6aa136ff86d2a1834f96500e347cd
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-09 06:45:36 +0000

    count similar letters

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -2,10 +2,15 @@
 #include <stdio.h>
 unsigned int _strspn(char *s, char *accept)
 {
+	int i = 0;
 	int count = 0;
-	if (*s == *accept)
+	while (s[i] != '\0')
 	{
-		count += count;
+	if (*s[i] == *accept)
+	{
+		count++;
+	}
+	i++;
 	}
 	return (count);
 }
