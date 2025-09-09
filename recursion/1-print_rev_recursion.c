e1b2b274c7ec4cbfa040b45d806f5a7b77467bbd
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-09 20:18:27 +0000

    print reverse

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "main.h"
+#include <unistd.h>
 void _print_rev_recursion(char *s)
 {
 	int i = 0;
 	while (s[i] != '\0')
 		i++;
-	printf("%d", s[i]);
+	 write(1, s, 1);
 	_print_rev_recursion(s - 1);
 }
 
