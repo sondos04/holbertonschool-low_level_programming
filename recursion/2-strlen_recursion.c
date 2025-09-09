9d817e10ffc35117daacbffceb199a42e6e24aa9
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-09 21:31:16 +0000

    print length of string

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,8 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: The string to measure
+ *
+ * Return: The length of the string
+ */
 int _strlen_recursion(char *s)
 {
-	while (*s != '\0')
-		return ((int) *s);
-	_strlen_recursion(s + 1);
+if (*s == '\0')
+	return 0;
+return 1 + _strlen_recursion(s + 1);
 }
