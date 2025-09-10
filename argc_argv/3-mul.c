b67cb2d5ee5dec6cabe156c20d4ac82ebd1f3697
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-10 13:10:42 +0000

    print multiplies two numbers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h> 
+/**
+ * main - multiplies all integer arguments passed to the program
+ * @argc: number of arguments passed to the program
+ * @argv: array of pointers to the arguments
+ *
+ * Description: This program takes integer arguments from the command line,
+ * multiplies them together, and prints the cumulative result after each multiplication.
+ * If no integers are provided, it prints an error message and returns 1.
+ *
+ * Return: 0 on success, 1 if no arguments are provided
+ */
 int main(int argc, char *argv[])
 {
-	int result;
-	if (argc[] == NULL)
+	int i;
+	int result = 1;
+	if (argc < 2)
 	{
-		printf("Error");
-		return (1);
+	printf("Error\n");
+	return (1);
 	}
-	else
+
+	for (i = 1; i < argc; i++)
 	{
-	result = argc[1] * argc[2];
-	printf("%d\n", result);
+		result = result * atoi(argv[i]);
+		printf("%d\n", result);
 	}
 	return (0);
 }
