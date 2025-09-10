5f8d7cd20a3dcdd7f1f444ec5f2e4c2fe687fbae
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-10 12:18:18 +0000

    print the number of argc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the program's own name
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments, argv[0] is the program name
+ *
+ * Prints the program's executable name as it was called
+ * from the terminal. Renaming or moving the executable
+ * changes the output automatically without recompiling.
+ *
+ * Return: return 0 .
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
