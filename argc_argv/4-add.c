dad8d0603ad84bef49501cd0e5a8f52ba9d563e5
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-10 13:54:05 +0000

    print sum of argc

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,35 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
 /**
- * main - sum all integer arguments passed to the program
+ * is_number - checks if a string represents a valid integer
+ * @s: string to check
+ *
+ * Return: 1 if valid integer, 0 otherwise
+ */
+int is_number(char *s)
+{
+	if (*s == '\0')
+	return (0);
+	if (*s == '-' || *s == '+')
+	s++;
+
+	if (*s == '\0')
+	return (0);
+
+	while (*s)
+	{
+	if (!isdigit(*s))
+	return (0);
+	s++;
+    	}
+	return (1);
+}
+/**
+ * main - sums all integer arguments passed to the program
  * @argc: number of arguments passed to the program
  * @argv: array of pointers to the arguments
  *
  * Description: This program takes integer arguments from the command line,
- * add them & print the result.
- * If no integers are provided, it prints an error message and returns 1.
+ * validates them, sums them, and prints the result.
+ * If any argument is not a valid integer or no arguments are provided,
+ * it prints "Error" and returns 1.
  *
- * Return: 0 on success, 1 if no arguments are provided or not corret.
+ * Return: 0 on success, 1 if an error occurs
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	int result = 0;
+	int i, result = 0;
 	
-	if (argc < 2)
-	{
-		printf("0\n");
-		return (0);
+    if (argc < 2)
+    	{
+	printf("Error\n");
+	return (1);
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if ((atoi(argv[i])) == 0)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		else
-			result = result + atoi(argv[i]);
+	if (!is_number(argv[i]))
+	{
+		printf("Error\n");
+		return (1);
+	}
+	result += atoi(argv[i]);
 	}
 	printf("%d\n", result);
 	return (0);
