9b919c1adb13145c23450baf29adfed3e871be86
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-10 12:50:10 +0000

    print multiplies two numbers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	int result;
+	if (argc[] == NULL)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
+	{
+	result = argc[1] * argc[2];
+	printf("%d\n", result);
+	}
+	return (0);
+}
