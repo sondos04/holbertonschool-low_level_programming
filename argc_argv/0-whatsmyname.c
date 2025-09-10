9b6fef9fad91567b40b4400386155766f2fe257d
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-10 12:01:17 +0000

    readme file for the directory

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
-int main (int argc, char *argv[])
+/*
+ * Function: main
+ * Arguments:
+ *   int argc   -> Number of arguments passed to the program
+ *   char *argv[] -> Array of strings containing the arguments
+ *                  argv[0] is always the program name (with path)
+ * Description:
+ *   Prints the program's own executable name as it was called
+ *   from the terminal. Renaming or moving the executable
+ *   changes the output automatically without recompiling.
+ */
+int main(int argc, char *argv[])
 {
 argc = 0;
 printf("%s\n", argv[argc]);
