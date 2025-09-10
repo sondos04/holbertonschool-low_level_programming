cf931d75c6b53bed99be0b234667b8c9518e2ae2
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-10 12:42:14 +0000

    print all argument

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - prints the program's argument
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments, argv[0] is the program name
+ *
+ * Prints the program's argument that recive
+ * Return: return 0.
+ */
 int main(int argc, char *argv[])
 {
 	int i;
