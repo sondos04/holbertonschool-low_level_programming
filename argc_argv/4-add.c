75240d67ab6329de699a5dc8d63c2f2e0f8a74bc
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-10 13:31:18 +0000

    print sum of argc

---
+++ argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies all integer arguments passed to the program
+ * @argc: number of arguments passed to the program
+ * @argv: array of pointers to the arguments
+ *
+ * Description: This program takes integer arguments from the command line,
+ * multiplies them & print the result.
+ * If no integers are provided, it prints an error message and returns 1.
+ *
+ * Return: 0 on success, 1 if no arguments are provided
+ */
+int main(int argc, char *argv[])
+{
+        int i;
+        int result = 1;
+	
+	if (argc < 2)
+        {
+        printf("0\n");
+        return (0);
+        }
+        for (i = 1; i < argc; i++)
+        {
+		if (argc < 48 || argc > 57)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+                result = result + atoi(argv[i]);
+        }
+        printf("%d\n", result);
+        return (0);
+}
