d3d09ce11a37b4df3ad03822627ff3e138c8312d
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-15 10:00:26 +0000

    create array by malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+char *create_array(unsigned int size, char c)
+{
+char *p;
+int i;
+if (size == 0)
+{
+return (NULL);
+}
+else
+{
+p = (char*) malloc(size * sizeof(char));
+for (i = 0 ; i < size ; i++)
+{
+p[i] = c;
+}
+}
+free(p);
+return (p);
+}
