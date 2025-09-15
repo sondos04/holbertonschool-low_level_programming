761097963d08b6a765183357d62b0668841b7d9b
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-15 19:33:39 +0000

    merg two array together

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+char char *str_concat(char *s1, char *s2)
+{
+char *p;
+int i;
+int j;
+int len1 = 0;
+int len2 = 0;
+if (s1 == NULL && s2 == NULL)
+{
+return (NULL);
+}
+while (s1[i] != '\0')
+{
+len1++;
+i++;
+}
+while (s2[j] != '\0')
+{
+len2++;
+j++;
+}
+p = malloc(((len1 + 1) + (len2 + 1)) * sizeof(char));
+if (p == NULL)
+return (NULL);
+i=0;
+j=0;
+
+while (s1[i] != '\0')
+{
+p[i] = s1[i];
+i++;
+}
+
+while (s2[j] != '\0')
+{
+p[i] = s2[j];
+i++;
+j++;
+}
+
+p[i] = '\0';
+return (p);
+}
