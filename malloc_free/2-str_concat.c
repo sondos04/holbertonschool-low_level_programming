929d17f9f905c0a7e33bdacb22589299e750da46
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-15 20:13:06 +0000

    merg two array together

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,10 +8,12 @@ int i = 0;
 int j = 0;
 int len1 = 0;
 int len2 = 0;
-if (s1 == NULL && s2 == NULL)
-{
-return (NULL);
-}
+
+
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 while (s1[i] != '\0')
 {
 len1++;
@@ -25,6 +27,7 @@ j++;
 p = malloc(((len1 + 1) + (len2 + 1)) * sizeof(char));
 if (p == NULL)
 return (NULL);
+
 i=0;
 j=0;
 
@@ -33,14 +36,12 @@ while (s1[i] != '\0')
 p[i] = s1[i];
 i++;
 }
-
 while (s2[j] != '\0')
 {
 p[i] = s2[j];
 i++;
 j++;
 }
-
 p[i] = '\0';
 return (p);
 }
