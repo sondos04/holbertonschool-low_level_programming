e8468458f2f0aa1731618f870c4e83bc3b109bca
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-17 13:14:32 +0000

    free a grid

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ *free_grid- free the grid after malloc
+ *@height: height of grid
+ *
+ *Return: nothing
+ */
 void free_grid(int **grid, int height)
 {
 int i;
