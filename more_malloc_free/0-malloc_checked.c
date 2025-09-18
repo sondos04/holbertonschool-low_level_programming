321204779882f4d2e56743e926d4b81f8bd183be
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-18 13:48:30 +0000

    malloc checked

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,10 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+/**
+ *malloc_checked- check the size
+ *@b: size of array
+ *
+ *Return: nothing
+ */
 void *malloc_checked(unsigned int b)
 {
 void *p = malloc(b);
-if(p == NULL)
+if (p == NULL)
 exit(98);
 return (p);
 }
