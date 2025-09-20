ddd9aba60585003838efd1fb2f7f87aed1a41200
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-20 18:02:35 +0000

    struct for name,age,owner

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog -struct for name, age, owner name
+ * @name: char
+ * @age: float
+ * @owner: char
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
 struct dog
 {
 char *name;
