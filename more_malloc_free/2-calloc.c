1819020d35e0125f0860d8b409390f14c1ada410
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-20 17:17:30 +0000

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ if (nmemb == 0)
 return (NULL);
 if (size == 0)
 return (NULL);
-p = malloc(nmemb *size);
+p = malloc(nmemb * size);
 if (p == NULL)
 return (NULL);
 for (i = 0; i < nmemb * size; i++)
