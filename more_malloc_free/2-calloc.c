d0dcbabaf464dff62c609fb3e6aeb54d2425ef47
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-20 17:16:23 +0000

    calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_calloc- create array of 0
+ *@nmemb: number of elements
+ *@size: size of elements
+ *
+ *Return: nothing
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *p;
+unsigned int i;
+if (nmemb == 0)
+return (NULL);
+if (size == 0)
+return (NULL);
+p = malloc(nmemb *size);
+if (p == NULL)
+return (NULL);
+for (i = 0; i < nmemb * size; i++)
+p[i] = 0;
+return (p);
+}
