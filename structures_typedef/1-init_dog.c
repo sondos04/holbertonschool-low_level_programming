50ddcf9c9764d530b437e985f88d9f1c725c67e4
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-20 18:25:45 +0000

    init parameters

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,7 +2,16 @@
 #include "dog.h"
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-d->name = name;
+if (d == NULL)
+return;
+
+d->name = malloc(strlen(name) + 1);
+if (d->name != NULL)
+strcpy(d->name, name);
+
+d->owner = malloc(strlen(owner) + 1);
+if (d->owner != NULL)
+strcpy(d->owner, owner);
+
 d->age = age;
-d->owner = owner;
 }
