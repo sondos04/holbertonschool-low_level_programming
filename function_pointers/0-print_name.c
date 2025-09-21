fcbb9952a45aa30c9b53b64be05d3693b30563d5
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-21 11:38:44 +0000

    function that prints a name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/*
+ *print_name- function that prints a name
+ *@name: pointer the name
+ *@f: pointer to function
+ *
+ *Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+	f(name);
+}
