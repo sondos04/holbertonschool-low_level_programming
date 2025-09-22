570859a2522edb42eada1745179f79f3dfd7b0a0
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-22 14:51:18 +0000

    scan array for sertin condition

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index- function scan every element in array for a condition
+ * @array: pointer to the array of integers
+ * @size: size of the array
+ * @cmp: pointer to the function to be executed
+ *
+ * Return: index if function true, or -1 otherwise
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (size <= 0)
+return (-1);
+
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+return (-1);
+}
