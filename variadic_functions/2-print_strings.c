4b06ee91f19ec824da62ee143500507f3b46472a
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-25 13:04:32 +0000

    print string by variadic_functions

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ *print_strings- print strings sperated by char
+ *@separator: pinter char
+ *@n: number of args
+ *@...: the strings
+ *
+ *Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+char *word;
+va_list args;
+
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+word = va_arg(args, char *);
+if (word != NULL)
+printf("%s", word);
+else
+printf("(nil)");
+
+if (separator != NULL && i != (n-1))
+{
+printf("%s", separator);
+}
+}
+va_end(args);
+printf("\n");
+}
