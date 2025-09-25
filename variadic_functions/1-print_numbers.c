4986b8c44307763619816614d9e9c12f66163986
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-25 12:55:17 +0000

    print args by variadic_functions

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
+/**
+ *print_numbers- print numbers sperated by char
+ *@separator: pinter char
+ *@n: number of args
+ *@...: the number should sum
+ *
+ *Return: nothing
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 unsigned int i;
