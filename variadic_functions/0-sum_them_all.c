e5bd730a3b729d0ab38ae9903727ffa4f2fe5dc8
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-25 12:39:59 +0000

    sum args by variadic_functions

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
+/**
+ *sum_them_all- sum all the args
+ *@n: number of args
+ *@...: the number should sum
+ *
+ *Return: return the sum of int, 0 if no args
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 unsigned int sum = 0;
