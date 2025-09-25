d63dd973c79d34b80660884a2a082090e5dc0f98
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-25 12:56:55 +0000

    print args by variadic_functions

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ for (i = 0; i < n; i++)
 num = va_arg(args, unsigned int);
 printf("%d", num);
 
-if (separator != NULL && i != (n-1))
+if (separator != NULL && i != (n - 1))
 {
 printf("%s", separator);
 }
