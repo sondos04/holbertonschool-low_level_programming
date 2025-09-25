2998bf47df9b485d35ae230332c3608d88e9d54d
sondos04 <sondos.alrubaih04@gmail.com>
2025-09-25 13:30:05 +0000

    print anything in proper type by variadic_functions

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -18,8 +18,9 @@ char *separator = "";
 va_start(args, format);
 
 while (format != NULL && format[i] != '\0')
-    {
-if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+{
+if (format[i] == 'c' || format[i] == 'i' || 
+	format[i] == 'f' || format[i] == 's')
 {
 printf("%s", separator);
 switch (format[i])
