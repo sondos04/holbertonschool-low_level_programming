2249b0dced65df07e405a94173f19803370d1608
sondos04 <sondos.alrubaih04@gmail.com>
2025-10-08 15:21:48 +0000

    print list content and length

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the first node
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+#include <stdio.h>
+#include "lists.h"
+
+size_t int count = 0;
+
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+eles
+printf("[%u] %s\n", h->len , h->str);
+h = h->next;
+count ++;
+}
