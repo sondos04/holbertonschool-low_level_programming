bf6cf7bd46383ea25064c3808733d57d74e04feb
sondos04 <sondos.alrubaih04@gmail.com>
2025-10-08 15:32:23 +0000

    main for liked list

--- singly_linked_lists/0-main.c
+++ singly_linked_lists/0-main.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-#include "main.h"
 /**
  * main - check the code
  *
--- singly_linked_lists/main.h
+++
@@ -1,4 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-size_t print_list(const list_t *h);
-#endif
