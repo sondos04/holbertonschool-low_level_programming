cd03dba6df8c56bf88ece071b711d95c0bc08111
sondos04 <sondos.alrubaih04@gmail.com>
2025-10-09 07:31:13 +0000

    count the length of list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - prints the length of a list_t list
+ * @h: pointer to the first node
+ *
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+size_t count = 0;
+
+while (h != NULL)
+{
+count++;
+}
+return (count);
+}
