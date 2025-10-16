dc37ed0dac5ca0a37974b61328d3431e964461b5
sondos04 <sondos.alrubaih04@gmail.com>
2025-10-16 10:25:46 +0000

    print dlistint -integeer-

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,3 +1,13 @@
+/**
+ * print_dlistint - prints all elements of a doubly linked list
+ * @h: pointer to the head of the list
+ *
+ * This function iterates through a doubly linked list
+ * and prints each integer element in a new line.
+ *
+ * Return: number of nodes printed
+ */
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -12,5 +22,5 @@ printf("%d\n", h->n);
 count++;
 h = h->next;
 }
-return(count);
+return (count);
 }
