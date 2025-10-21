8d9cf4711acf8cff41ff1e7557433d61c34f3b0c
sondos04 <sondos.alrubaih04@gmail.com>
2025-10-21 22:16:29 +0000

    make the betty style and documentation

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,6 +2,14 @@
 #include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
+
+/**
+ *hash_table_create - creat a an emty table of size @size 
+ *@size: inteeger of the size of array
+ *
+ *Return: table for sucseess, NULL for failer
+ *
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *table;
@@ -9,14 +17,14 @@ hash_table_t *table;
 if (size == 0)
 return (NULL);
 
-table = calloc(1,sizeof(hash_table_t));
+table = calloc(1, sizeof(hash_table_t));
 if (table == NULL)
 return (NULL);
 table->size = size;
-table->array = calloc(size,sizeof(hash_node_t*));
-if(table->array == NULL)
+table->array = calloc(size, sizeof(hash_node_t *));
+if (table->array == NULL)
 {
-free (table);
+free(table);
 return (NULL);
 }
 return (table);
