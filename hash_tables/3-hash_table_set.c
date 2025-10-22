da03165d475dc90fa20161e38f5114911f715f09
sondos04 <sondos.alrubaih04@gmail.com>
2025-10-22 18:58:15 +0000

    update the betty style

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+
 /**
  * hash_table_set - Add or update a key/value pair in a hash table
  * @ht: pointer to the hash table
@@ -9,49 +10,54 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-hash_node_t *node;
-char *value_copy;
-unsigned long int index;
+	hash_node_t *node;
+	char *value_copy;
+	unsigned long int index;
 
-if (ht == NULL || key == NULL || value == NULL || key[0] == '\0')
-return (0);
+	if (ht == NULL || key == NULL || value == NULL || key[0] == '\0')
+		return (0);
 
-index = key_index((const unsigned char *)key, ht->size);
-node = ht->array[index];
-while (node)
-{
-if (strcmp(nde->key, key) == 0)
-{
-value_copy = strdup(value);
-if (value_copy == NUll)
-return (0);
+	index = key_index((const unsigned char *)key, ht->size);
 
-free(node->value);
-node->value = value_copy;
-return (1);
-}
-node = node->next;
-}
+	/* Update path if key already exists */
+	node = ht->array[index];
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0) /* <-- كان مكتوب nde */
+		{
+			value_copy = strdup(value);
+			if (value_copy == NULL)       /* <-- كان مكتوب NUll */
+				return (0);
 
-node = malloc(sizeof(*node));
-if (node == NULL)
-return (0);
+			free(node->value);
+			node->value = value_copy;
+			return (1);
+		}
+		node = node->next;
+	}
 
-node->key = strdup(key);
-if (node->key == NULL)
-{
-free(node);
-return (0);
-}
-node->value = strdup(value);
-if (node->value == NULL)
-{
-free(node->key);
-free(node);
-return (0);
-}
+	/* Insert new node at head (chaining) */
+	node = malloc(sizeof(*node));
+	if (node == NULL)
+		return (0);
 
-node->next = ht->array[index];
-ht->array[index] = node;
-return (1);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (0);
+	}
+
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (0);
+	}
+
+	node->next = ht->array[index];
+	ht->array[index] = node;
+
+	return (1);
 }
