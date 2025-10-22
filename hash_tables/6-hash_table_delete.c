8ff8ff8e98c38ed2292dd5bce166d15d3124d090
sondos04 <sondos.alrubaih04@gmail.com>
2025-10-22 21:19:05 +0000

    function to delete a hash table

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -3,7 +3,7 @@
 /**
 * hash_table_delete -  delet a hash table
 * @ht: pointer to the hash table
-* 
+*
 * Return: nothing
 */
 void hash_table_delete(hash_table_t *ht)
@@ -14,7 +14,7 @@ unsigned long int i;
 if (ht == NULL)
 return;
 
-for(i = 0; i < ht->size; i++)
+for (i = 0; i < ht->size; i++)
 {
 node = ht->array[i];
 while (node)
@@ -23,9 +23,9 @@ tmp = node->next;
 free(node->key);
 free(node->value);
 free(node);
-node =tmp;
+node = tmp;
 }
 }
 free(ht->array);
 free(ht);
-}                                                                                          
+}
